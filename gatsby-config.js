5121c8b7a4ecaa2736ddb33d237b24af8bc05445
James Curtin <jameswcurtin@gmail.com>
2021-09-11 15:20:23 -0400

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
-                icon: 'src/images/icon.png',
+                icon: 'src/images/rings.png',
             },
         },
         'gatsby-plugin-mdx',
