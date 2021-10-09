dc42f34fd1550e9a3572184e5faa1c5758fa0760
James Curtin <jameswcurtin@gmail.com>
2021-10-09 13:19:46 -0400

    Add photo to homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { mainBody, weddingInfo } from './home.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
+
+import { mainBody, saveTheDatePhoto, weddingInfo } from './home.module.css'
 import Config from '../utils/config'
 import Layout from './layout'
 
@@ -12,6 +14,14 @@ export default function Home() {
             <p className={weddingInfo}>{Config.wedding.date}</p>
             <p className={weddingInfo}>{Config.wedding.location}</p>
             <p className={weddingInfo}>{Config.wedding.town}</p>
+            <StaticImage
+                src="../images/save-the-date.jpg"
+                alt="James and Elize save the date photo"
+                width={400}
+                className={saveTheDatePhoto}
+                placeholder="blurred"
+            />
+            <br />
             <br />
             <p className={mainBody}>
                 Still under construction - check back for more updates soon!
