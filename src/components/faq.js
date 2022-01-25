e0f1aa470c303bb373c3d971a4afc0d20ddf724b
James Curtin <jameswcurtin@gmail.com>
2022-01-24 23:39:05 -0500

    Formatting

--- src/components/faq.js
+++ src/components/faq.js
@@ -9,14 +9,18 @@ export default function Faq() {
         <Layout pageTitle="Frequently Asked Questions">
             <p className={mainQuestion}>When is the ceremony?</p>
             <p className={mainAnswer}>
-                We were married in a small ceremony at the Arnold Arboretum on November 13, 2021. See the gallery tab for some of our favorite pictures from the day. We look
-                forward to celebrating our marriage with you all on April 9, 2022!
+                We were married in a small ceremony at the Arnold Arboretum on
+                November 13, 2021. See the gallery tab for some of our favorite
+                pictures from the day. We look forward to celebrating our
+                marriage with you all on April 9, 2022!
             </p>
             <br />
             <p className={mainQuestion}>What are your Covid safety plans?</p>
             <p className={mainAnswer}>
-                We are requiring everyone intending to attend to be fully vaccinated. 
-                We want to celebrate our wedding with as many of our loved ones as possible, but we also want to be as safe as possible. Thank you for understanding!
+                We are requiring everyone intending to attend to be fully
+                vaccinated. We want to celebrate our wedding with as many of our
+                loved ones as possible, but we also want to be as safe as
+                possible. Thank you for understanding!
             </p>
         </Layout>
     )
